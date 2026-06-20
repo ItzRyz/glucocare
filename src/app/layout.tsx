2f5ee3ad97b2826a0dfe712f308d02721da0f69a
ItzRyz <zetsura.official04@gmail.com>
2026-06-20 09:19:42 +0700

    fix: Frontend components end configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,10 @@
 import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/layout/nav-bar";
 import { cn } from "@/lib/utils";
-const poppins = Poppins({
-  variable: "--font-poppins",
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700"],
-});
+import Navbar from "@/components/layout/nav-bar";
+
+const montserrat = Montserrat({ subsets: ['latin'], variable: '--font-sans' });
 
 export const metadata: Metadata = {
   title: "GlucoCare - Diabetic Diagnose & Telemedicine",
@@ -22,9 +19,9 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className="h-full antialiased"
+      className={cn("h-full", "antialiased", montserrat.variable)}
     >
-      <body className={cn(poppins.className, "min-h-full flex flex-col bg-background text-foreground")}>
+      <body className={cn(montserrat.className, "min-h-full flex flex-col bg-background text-foreground")}>
         <Navbar />
         <main className="flex-1">
           {children}
