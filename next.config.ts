cf9b89b291c3d3d32daf073447ea72b5f75ccefd
ItzRyz <zetsura.official04@gmail.com>
2026-06-21 17:30:09 +0700

    chore: migrate backend service from vercel.json to custom next.js rewrites and add flask development script

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/flask/:path*",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:5328/api/flask/:path*"
+            : "/api/index",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
