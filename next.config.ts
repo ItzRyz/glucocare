e8ac8b852c1a92abe5eccc2e8fba9cea334dbfd2
ItzRyz <zetsura.official04@gmail.com>
2026-06-21 17:13:27 +0700

    refactor: migrate backend to experimental services and configure environment settings

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
-  async rewrites() {
-    return [
-      {
-        source: "/api/flask/:path*",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:5328/api/flask/:path*"
-            : "/api/index.py",
-      },
-    ];
-  },
 };
 
 export default nextConfig;
